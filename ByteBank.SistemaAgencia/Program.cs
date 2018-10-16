fc8e4a4cfeb6f2b90304e62530f2bfc0197716e9
BruLekichLouro <bububio@yahoo.com.br>
2018-10-16 12:45:03 -0300

    Iniciando arquivos e sistemas

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(847, 489754);
+
+            Funcionario carlos = new Desenvolvedor("312666");
+           
+            Console.WriteLine(conta.Numero);
+            Console.WriteLine(conta.Saldo);
+            Console.ReadLine();
+        }
+    }
+}
