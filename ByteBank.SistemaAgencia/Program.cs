38d490e22a652c3f13c4e03368761dd33f42bc85
BruLekichLouro <bububio@yahoo.com.br>
2018-11-05 18:16:04 -0200

    Implementando lista de contas correntes

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -14,7 +14,19 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-           
+            ListaDeContaCorrente lista = new ListaDeContaCorrente();
+            lista.Adicionar(new ContaCorrente(453, 234555));
+            lista.Adicionar(new ContaCorrente(455, 234855));
+            lista.Adicionar(new ContaCorrente(455, 234851));
+            lista.Adicionar(new ContaCorrente(495, 234858));
+            lista.Adicionar(new ContaCorrente(475, 234859));
+            lista.Adicionar(new ContaCorrente(435, 234856));
+            lista.Adicionar(new ContaCorrente(425, 234854));
+            lista.Adicionar(new ContaCorrente(445, 234853));
+            lista.Adicionar(new ContaCorrente(145, 235853));
+            lista.Adicionar(new ContaCorrente(545, 234893));
+            lista.Adicionar(new ContaCorrente(345, 134853));
+
             Console.ReadLine();
 
         }
