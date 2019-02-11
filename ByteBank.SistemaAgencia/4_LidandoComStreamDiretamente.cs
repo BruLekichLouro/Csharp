adcb1e1e76b8aa1242c9999fa7bd3929d6ecfe76
BruLekichLouro <bububio@yahoo.com.br>
2019-02-11 14:21:01 -0200

    Utilizando o StreamReader

--- ByteBank.SistemaAgencia/4_LidandoComStreamDiretamente.cs
+++ ByteBank.SistemaAgencia/4_LidandoComStreamDiretamente.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Comparadores;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    partial class Program
+    {
+        private static void LidandoComStreamDiretamente()
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+                var buffer = new byte[1024];
+
+                var numeroDeBytesLidos = -1;
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
+
+                }
+                }
+            }
+            static void EscreverBuffer(byte[] buffer, int byteslidos)
+            {
+                var utf8 = new UTF8Encoding();
+                var texto = utf8.GetString(buffer, 0, byteslidos);
+                Console.Write(texto);
+
+            }
+        }
+    }
